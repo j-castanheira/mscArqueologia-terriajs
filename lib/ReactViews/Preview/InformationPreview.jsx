63894c8ed58d058736c92e3617078a7fed099e80
José Castanheira <j.castanheira@campus.fct.unl.pt>
2018-10-28 19:46:06 +0000

    Slideshow added

--- lib/ReactViews/Preview/InformationPreview.jsx
+++ lib/ReactViews/Preview/InformationPreview.jsx
@@ -4,7 +4,7 @@ import ObserveModelMixin from '../ObserveModelMixin';
 import React from 'react';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
-import Styles from './mappable-preview.scss';
+import Styles from './information-preview.scss';
 
 /**
  * Information preview section
@@ -13,15 +13,24 @@ const InformationPreview = createReactClass({
     displayName: 'InformationPreview',
     mixins: [ObserveModelMixin],
 
+
     propTypes: {
         terria: PropTypes.object.isRequired,
         viewState: PropTypes.object,
         previewed: PropTypes.string
     },
 
+    getInitialState(){
+        return{
+            currentImg: 0,
+            timeToNext: 3000,
+            images: this.props.viewState.currentItem.resources.map(image => image.url)
+        };
+    },
+
     getInfo(info)
     {
-        let infoList = '';
+        let infoList = [];
         let i,j;
         if(info !=undefined) {
             for (i = 0; i < info.length; i++) {
@@ -29,7 +38,7 @@ const InformationPreview = createReactClass({
                     var p = info[i].text;
                     if (p != undefined)
                         for (j = 0; j < p.length; j++) {
-                            infoList += "<li>" + p[j] + "</li>"
+                            infoList.push(String(p[j]));
                         }
                 }
                 else continue
@@ -44,6 +53,28 @@ const InformationPreview = createReactClass({
         window.open(this.props.viewState.currentItem.sourcePage[0]);
     },
 
+    changeImg()
+    {
+        var img = this.state.currentImg;
+        if (img < this.state.images.length-1){img++} else {img=0}
+        this.setState({currentImg: img});
+        console.log("CURR",this.state.currentImg);
+    },
+
+    jumpToSlide(index) {
+        console.log("jump");
+        this.setState({ currentImg: index });
+    },
+
+    componentDidMount(){
+        let intervalId = setInterval(this.changeImg,this.state.timeToNext);
+        this.setState({ intervalId: intervalId })
+    },
+
+    componentWillUnmount(){
+        clearInterval(this.state.intervalId);
+    },
+
     render() {
         const previewed = this.props.previewed;
         const info = this.props.viewState.currentItem[previewed];
@@ -52,9 +83,11 @@ const InformationPreview = createReactClass({
         //const image = this.props.viewState.currentItem.resources[0].url;
         console.log(info);
         const infoList = this.getInfo(info);
-
-
         console.log(infoList);
+
+        //var images = this.props.viewState.currentItem.resources.map(image => image.url );
+        //console.log("IMGS",images);
+        console.log("img",this.state.images[this.state.currentImg]);
 
         return (
             <div className={Styles.preview}>
@@ -64,10 +97,24 @@ const InformationPreview = createReactClass({
                         {'Visit ' + repository + ' page'}
                     </button>
                     <div className={Styles.previewedInfo}>
-                        <h3 className={Styles.h3}>{name}</h3>
-                        <ul>
-                            {infoList}
+                        <img name="slide" src={this.state.images[this.state.currentImg]} height="200" width="200"></img>
+                        <ul className="slideshow-dots">
+                            {
+                                this.state.images.map((slide, index) => (
+                                    <li className={ (index == this.state.currentImg) ? 'active': '' } key={index}>
+                                        <a onClick={ (event)=> this.jumpToSlide(index) }>{ index + 1 }</a>
+                                    </li>
+                                ))
+                            }
                         </ul>
+                        <h3 className={Styles.h3}>{name}</h3>
+                        <div>
+                            <ul>
+                            {
+                                infoList.map((item, index) => <li key={index}>{item}</li>)
+                            }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
